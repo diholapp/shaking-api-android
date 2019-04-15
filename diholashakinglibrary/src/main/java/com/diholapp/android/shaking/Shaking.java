4746780afc935dcee598c5f6127090f9abca24e2
DiHola S.L <info@diholapp.com>
2019-04-15 12:50:27 +0200

    Initial Commit

---
+++ diholashakinglibrary/src/main/java/com/diholapp/android/shaking/Shaking.java
@@ -0,0 +1,14 @@
+package com.diholapp.android.shaking;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Shaking {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+
+}
